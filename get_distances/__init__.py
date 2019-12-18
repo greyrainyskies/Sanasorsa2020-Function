e49f1e60e8005b0dcfbe63493d548d92eb893945
unie31 <urho.niemela@outlook.com>
2019-12-18 15:34:15 +0200

    score calculation function added

--- get_distances/__init__.py
+++ get_distances/__init__.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+import math
 
 import azure.functions as func
 
@@ -29,7 +30,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         for guessed_word in guesses:
             # Check if guess is in the model, otherwise return 0
             if guessed_word in wv:
-                similarities.append(str(wv.similarity(original, guessed_word)))
+                similarities.append(str(CalculateScore(original, guessed_word)))
+                # similarities.append(str(wv.similarity(original, guessed_word)))
             else:
                 similarities.append(str(0))
         return func.HttpResponse(
@@ -39,3 +41,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             )
     else:
         return func.HttpResponse("The format of the JSON request was invalid.", status_code=400)
+
+
+def CalculateScore(original, guess):
+    angle = math.degrees(math.acos(model.wv.similarity(original, guess)))
+
+    if angle != 0:
+            score = 100000000/(angle**3)
+        else:
+            score = 0
+
+    return score
